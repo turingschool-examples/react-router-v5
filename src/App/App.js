efa2ae079f6c096fba6e82cd05e0486f729b477a
Khalid Williams <khalidwill27@gmail.com>
2020-05-12 10:45:46 -0600

    Add boilerplate for Creatures, App and Home

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,11 @@ export default class App extends Component {
   render() {
     return (
       <main className="App">
-        Hello World!
+        <nav>
+          <a href="/puppies" className="nav">Puppies</a>
+          <a href="/sharks" className="nav">Sharks</a>
+        </nav>
+        <h1>Puppies or Sharks</h1>
       </main>
     );
   }
---
+++ src/Creatures/Creatures.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './imageDisplay.css';
+
+const Creatures = ({ data, name}) => {
+
+}
+
+export default Creatures;
